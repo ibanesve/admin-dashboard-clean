ad9d635f17fc7eff48c22c968c44c86881a8594b
ibanesve <ibanesve@yahoo.com>
2025-07-11 13:44:35 -0700

    Update test-admin.jsx

--- pages/admin/test-admin.jsx
+++ pages/admin/test-admin.jsx
@@ -1,6 +1,6 @@
-import AddLocationForm from "../components/AddLocationForm";
+import AddLocationForm from "@/components/AddLocationForm";
 
-export default function TestAdminPage() {
+export default function AdminPage() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">MexCamino Admin Panel</h1>
